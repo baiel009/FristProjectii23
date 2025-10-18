3a5d7fbe142414b8247a53e1410ac3ecef001bc4
baiel009 <dujsegaziev@gmail.com>
2025-10-18 15:56:30 +0600

    new folder

--- name.py
+++ name.py
@@ -1,6 +1,3 @@
-words = ('kazak', 'car', 'laptop', 'madam', 'kg')
-new_words = []
-for n in words:
-    if n == n[::-1]:
-        new_words.append(n)
-print(new_words)
+12261234567890-0987654321
+
+84873623653843498
